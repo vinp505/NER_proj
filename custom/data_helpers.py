4ee269e3c2f6c74b64fc33e348e01f1758deaa36
vinp505 <pirasvincenzo05@gmail.com>
2026-04-16 16:08:06 +0200

    Added raw sentence storing and retrieval system to the data loader class. Added stratified sampling for the data splitter class.

--- custom/data_helpers.py
+++ custom/data_helpers.py
@@ -5,6 +5,7 @@ Helper script to define classes related to data loading, processing and handling
 # ------------------------------------------------------------
 
 import requests
+import random
 import torch
 import numpy as np
 
@@ -47,6 +48,9 @@ class LanguageData():
 
     **get_lang_data(lang)**
         Retrieves the list containing Dataset objects for the specified language.
+
+    **get_lang_sentences(lang)**
+        Retrieves the list containing sentences (split into words) for the specified language.
     
     **get_smallest_set_size(set)**
         Retrieves the size of the smallest set of the specified type across all languages, together with its language code.
@@ -128,26 +132,37 @@ class LanguageData():
         # initialize dict to store language Dataset objects
         self.lang2data = {}
 
+        # initialize dict to store language sentences
+        self.lang2sent = {}
+
         # iterate through languages
         for lang, url in lang2link.items():
             
             if verbose:
                 print(f"\nLoading '{lang}' data ...")
 
+
             # set up collections for data
+
             lang_datasets = {
                 "train" : None,
                 "test" : None
             }
 
+            lang_sentences = {
+                "train" : None,
+                "test" : None
+            }
+
             tmp_datasets = []
+            tmp_sentences = []
 
             # iterate through data files (train, dev, test)
             for data_set in data_sets:
 
                 # obtain complete file link and attempt download
                 file_url = url + data_set
-                sentences, labels, status_code = self._load_iob(file_url)
+                raw_sentences, sentences, labels, status_code = self._load_iob(file_url)
 
                 # if download fails on any of train, dev, test sets, store the info
                 if status_code != 200:
@@ -162,6 +177,7 @@ class LanguageData():
                     # tokenize sentences, align labels, obtain and store Dataset
                     lang_data = self._create_Dataset(sentences, labels, self.model, verbose)
                     tmp_datasets.append(lang_data)
+                    tmp_sentences.append(raw_sentences)
             
             if verbose:
                 print(f"Total data sets for '{lang}': {len(tmp_datasets)}.")
@@ -175,23 +191,37 @@ class LanguageData():
                 if verbose:
                    print(f"\nOnly one file successfully loaded for '{lang}'. It will be split into train ({tr*100}%) and test ({(1-tr)*100}%) sets.")
 
+                
                 # split
+                
                 d = tmp_datasets[0]
                 train_set_size = int(tr*d.shape[0])
                 train_set = d.select(range(train_set_size))
                 test_set = d.select(range(train_set_size, d.shape[0]))
 
+                s = tmp_sentences[0]
+                train_sentences = s[:train_set_size]
+                test_sentences = s[train_set_size:]
+
+
                 # update language datasets
                 lang_datasets["train"] = train_set
                 lang_datasets["test"] = test_set
+
+                lang_sentences["train"] = train_sentences
+                lang_sentences["test"] = test_sentences
 
                 if verbose:
                     print(f"Successfully created train ({train_set.shape[0]} sentences) and test ({test_set.shape[0]} sentences) sets for '{lang}'.")
 
             # if only two files were loaded, use for train, and test
             if len(tmp_datasets) == 2:
+
                 lang_datasets["train"] = tmp_datasets[0]
                 lang_datasets["test"] = tmp_datasets[1]
+
+                lang_sentences["train"] = tmp_sentences[0]
+                lang_sentences["test"] = tmp_sentences[1]
 
             # if all three data sets were loaded, merge appropriate data sets
             if len(tmp_datasets) == 3:
@@ -199,10 +229,15 @@ class LanguageData():
                 if verbose:
                     print(f"\nAll three files successfully loaded for '{lang}'. Data will be merged.")
                 
-                # train and dev sets are needed
+                # train and test sets are needed
                 train_dev_set = concatenate_datasets([tmp_datasets[0], tmp_datasets[1]], axis= 0)
+                train_dev_sentences = tmp_sentences[0] + tmp_sentences[1]
+
                 lang_datasets["train"] = train_dev_set
                 lang_datasets["test"] = tmp_datasets[2]
+  
+                lang_sentences["train"] = train_dev_sentences
+                lang_sentences["test"] = tmp_sentences[2]
 
                 if verbose:
                     print(f"Successfully merged train and dev sets. New training set contains {lang_datasets["train"].shape[0]} sentences.")
@@ -232,8 +267,9 @@ class LanguageData():
 
             # link all downloaded datasets to proper language
             self.lang2data[lang] = lang_datasets
+            self.lang2sent[lang] = lang_sentences
 
-    def get_lang_data(self, lang: str) -> dict[Dataset]:
+    def get_lang_data(self, lang: str) -> dict[str, Dataset]:
         """
         Retrieves the dictionary containing Dataset objects for the specified language code.
         Consult the supported language codes in the class documentation.
@@ -253,6 +289,27 @@ class LanguageData():
 
         # return list of Dataset objects for the given language
         return self.lang2data[lang]
+
+    def get_lang_sentences(self, lang: str) -> dict[str, list[list[str]]]:
+        """
+        Retrieves the dictionary containing metadata and individual words for each sentence for the specified language code.
+        Consult the supported language codes in the class documentation.
+
+        Parameters
+        ----------
+
+        lang : str
+            The language code corresponding to the needed sentence data.
+        
+        Returns
+        -------
+
+        lang_sentences : dict[list[list[str]]]
+            The dictionary containing the Dataset objects for the specified language.
+        """
+
+        # return list of Dataset objects for the given language
+        return self.lang2sent[lang]
     
     def get_smallest_set_size(self, set: Literal["train", "test", "all"] = 'all') -> tuple[str, Dataset] | dict[str, tuple[str, Dataset]]:
         """
@@ -325,6 +382,7 @@ class LanguageData():
         response = requests.get(url)
 
         # initialize lists to store sentences and relative labels
+        raw_sentences = [[]]  # stores metadata about the sentence (ID, text)
         sentences = [[]]
         labels = [[]]
 
@@ -336,27 +394,33 @@ class LanguageData():
 
                 # end of sentence line: add new sentence list
                 if str(line) == "b''" or str(line) == "\n":
+                    raw_sentences.append([])
                     sentences.append([])
                     labels.append([])
-                
-                # if non-empty line and not a comment
-                elif (str(line)[0] != '#') and (str(line)[0:3] != "b'#") and (str(line)[0:3] != """b"#"""):
 
+                # if not empty line but a comment
+                elif (str(line)[0] == '#') or (str(line)[0:3] == "b'#") or (str(line)[0:3] == """b"#"""):
+                    raw_sentences[-1].append(str(line.decode('utf-8')))
+
+                # annotated word
+                else:
                     # split line: word_number, word, label_1, label_2, label_3
                     split_line = str(line.decode('utf-8')).split('\t')
                     word = split_line[1]
                     label = split_line[2]
 
                     # store word and label
+                    raw_sentences[-1].append(word)
                     sentences[-1].append(word)
                     labels[-1].append(label)
         
         # remove redundant new sentences (if file ends in multiple new lines)  
         while sentences[-1] == [] and len(sentences) > 1:
+            raw_sentences = raw_sentences[:-1]
             sentences = sentences[:-1]
             labels = labels[:-1]
         
-        return sentences, labels, response.status_code
+        return raw_sentences, sentences, labels, response.status_code
     
     def _create_Dataset(self, sentences: list, labels: list, model: str, verbose: bool) -> Dataset:
         """
@@ -457,10 +521,6 @@ class DataSplit():
     k : int, optional (default= 10)
         Amount of sequences to select from the train set of non-target languages.
         Must be less than or equal to the size of the smallest train set recorded.
-
-    random_state : int | None, optional (default= None)
-        Random state seed to ensure deterministic train and test sets.
-        If set to 0 (zero), no shuffling will occur.
     
     Methods
     --------
@@ -468,12 +528,19 @@ class DataSplit():
     **get_train_set()**
         Retrieves the train set.
     
+    **get_train_sentences()**
+        Retrieves the train sentences list.
+    
     **get_test_set(lang)**
         Retrieves the dictionary containing lang - test_set pairs, or,
         if a specific language is given, the relative test set Dataset object.
+
+    **get_test_sentences(lang)**
+        Retrieves the dictionary containing lang - test_sentences pairs, or,
+        if a specific language is given, the relative list of test senteces.
     """
         
-    def __init__(self, langData: LanguageData, target_lang: str = "all", k: int = 10, random_state: int | None = None):
+    def __init__(self, langData: LanguageData, target_lang: str = "all", k: int = 10):
         
         # ensure the target language is among the ones considered target language when loading the data
         assert (target_lang in langData.target_langs) or target_lang == 'all', f"The given target language {target_lang} is not present in the accepted target language list of the LanguageData object {langData.target_langs}"
@@ -485,22 +552,27 @@ class DataSplit():
         self.langData = langData
         self.target_lang = target_lang
         self.k = k
-        self.random_state = random_state
 
         # initialize train set, and test set dictionary
         self.train_set = []
         self.test_sets = {}
 
+        self.train_sent = []
+        self.test_sent = {}
+
         # iterate through the languages
         for lang in self.langData.lang_codes:
             
             # obtain train and test sets
-            train_set_lang = self._obtain_train_set(lang)
-            test_set = self._obtain_test_set(lang)
+            train_set_lang, train_sent_lang = self._obtain_train_set(lang)
+            test_set, test_sent = self._obtain_test_set(lang)
 
             # store data
             self.train_set.append(train_set_lang)
             self.test_sets[lang] = test_set
+
+            self.train_sent.extend(train_sent_lang)
+            self.test_sent[lang] = test_sent
         
         # only one train set is needed, concatenate individual sets into one
         self.train_set = concatenate_datasets(self.train_set, axis= 0)
@@ -517,6 +589,18 @@ class DataSplit():
             The train set.
         """
         return self.train_set
+    
+    def get_train_sentences(self) -> list[list[str]]:
+        """
+        Retrieves the train sentences.
+
+        Returns
+        -------
+
+        train_sent : list[list[str]]
+            The list of train sentences split into lists of words.
+        """
+        return self.train_sent
     
     def get_test_set(self, lang: str = 'all') -> dict[str, Dataset] | Dataset:
         """
@@ -546,6 +630,35 @@ class DataSplit():
         
         # return language specific test set
         return self.test_sets[lang]
+    
+    def get_test_sentences(self, lang: str = 'all') -> dict[str, list[list[str]]] | list[list[str]]:
+        """
+        Retrieves the dictionary containing lang - test_sentences pairs, or,
+        if a specific language is given, the relative list of test senteces.
+
+        Parameters
+        ----------
+
+        lang : str, optional (default= 'all')
+            The language whose test set is to be retrieved.
+            By default, the dictionary containing lang - test_set pairs
+            for each language will be returned instead.
+        
+        Returns
+        -------
+
+        test_sent : dict[str, list[list[str]]] | list[list[str]]
+            If no language is specified, the dictionary containing 
+            lang - test_sentences pairs for each language.
+            Else, the language specific list of test senteces.
+        """
+
+        # return entire dictionary
+        if lang == 'all':
+            return self.test_sent
+        
+        # return language specific test set
+        return self.test_sent[lang]
 
 
     def _obtain_train_set(self, lang) -> Dataset:
@@ -559,42 +672,30 @@ class DataSplit():
         lang_data = self.langData.get_lang_data(lang)
         train_set = lang_data["train"]
 
+        lang_sent = self.langData.get_lang_sentences(lang)
+        train_sent = lang_sent["train"]
+
         # no target language -> all train sets of equal proportions
+        # redute train set to size of the smallest train set
         if self.target_lang == "all":
-
-            # shuffle if needed
-            if self.random_state != 0:
-                train_set = train_set.shuffle(seed= self.random_state)
-
-            # redute train set to size of the smallest train set
-            smallest_train_set = self.langData.get_smallest_set_size("train")[1]
-            sampled_train_set = train_set.select(range(smallest_train_set))
+            size = self.langData.get_smallest_set_size("train")[1]
         
         # target language
+        # reduce train set to the size of the smallest target language train set
         elif lang == self.target_lang:
-
-            # shuffle if needed
-            if self.random_state != 0:
-                train_set = train_set.shuffle(seed= self.random_state)
-
-            # reduce train set to the size of the smallest target language train set
-            smallest_target_train_set = self.langData.get_smallest_target_set_size("train")[1]
-            sampled_train_set = train_set.select(range(smallest_target_train_set))
+            size = self.langData.get_smallest_target_set_size("train")[1]
         
         # not a target language
+        # reduce train set to contain k samples
         else:
-            
-            # shuffle if needed
-            if self.random_state != 0:
-                train_set.shuffle(seed= self.random_state)
-
-            # reduce train set to contain k samples
-            sampled_train_set = train_set.select(range(self.k))
+            size = self.k
         
-        return sampled_train_set
+        # obtain data sample
+        sampled_train_set, sampled_train_sent = self._sample_data(train_set, train_sent, size)
+        return sampled_train_set, sampled_train_sent
 
 
-    def _obtain_test_set(self, lang) -> Dataset:
+    def _obtain_test_set(self, lang: str) -> Dataset:
         """
         Internal function: retrieve language test set,
         and resize it to have the same amount of sequences
@@ -605,14 +706,94 @@ class DataSplit():
         lang_data = self.langData.get_lang_data(lang)
         test_set = lang_data["test"]
 
-        # shuffle if needed
-        if self.random_state != 0:
-            test_set.shuffle(seed= self.random_state)
-        
+        lang_sent = self.langData.get_lang_sentences(lang)
+        test_sent = lang_sent["test"]
+
         # reduce test set to size of the smallest train set
-        smallest_test_set = self.langData.get_smallest_set_size("test")[1]
-        sampled_test_set = test_set.select(range(smallest_test_set))
+        size = self.langData.get_smallest_set_size("test")[1]
+        sampled_test_set, sampled_test_sent = self._sample_data(test_set, test_sent, size)
         
-        return sampled_test_set
+        return sampled_test_set, sampled_test_sent
+    
+    def _sample_data(self, data: Dataset, sentences: list[list[str]], size: int) -> Dataset:
+        """
+        Internal function: obtain a sample of the given data of the given size,
+        ensuring entity density is preserved.
+        """
+
+        # if size is equal to amt of data, just return the data
+        if size == data.shape[0]:
+            return data, sentences
+        
+        # compute entity density by counting total amt of tokens and of entities
+        tot_tokens = sum([len(sample["labels"]) for sample in data])  # !!! THIS COUNTS END OF SENTENCE PADDING TOKENS BUT IT IS FINE
+        tot_entities = sum([sum([1 for x in sample["labels"] if (x != -100) and (x != 0)]) for sample in data])#sum up all the entities
+        density = tot_entities / tot_tokens
+
+        # obtain amount of required densities to preserve density
+        req_entities = int(size * 64 * density)
+
+        print(f"density: {density}")
+        print(f"req_entities: {req_entities}")
+
+        # split data into sentences containing entities and sentences not containing any
+        dataset_ent = data.filter(lambda row: any((tag != -100) and (tag != 0) for tag in row["labels"]))
+        dataset_no_ent = data.filter(lambda row: all((tag == -100) or (tag == 0) for tag in row["labels"]))
+
+        # initialize list to split data
+        sent_ent = []
+        sent_no_ent = []
+
+        # keep track of total entities encountered and amount of sentences needed for it
+        tot_entities = 0
+        amt_needed_sentences = 0
+
+        # iterate through data
+        for i, sample in enumerate(data):
+            
+            # compute amount of entities in current sentence
+            amt_entities = sum((tag != -100) and (tag != 0) for tag in sample["labels"])
+
+            # if any entities are present
+            if amt_entities:
+
+                # store in appropriate list
+                sent_ent.append(sentences[i])
+
+                # if more entities are still needed update counters
+                if tot_entities < req_entities:
+                    amt_needed_sentences += 1
+                    tot_entities += amt_entities
+            
+            # if no entities are present
+            else:
+                sent_no_ent.append(sentences[i])
+
+        print(f"Sentences with entities: {len(sent_ent)}")
+        print(f"Sentences with no entities: {len(sent_no_ent)}")
+        print(f"Amt. needed sentences with entities: {amt_needed_sentences}")
+
+        # only retain the amount of sentences needed to preserve the entity density
+        sampled_data_ent = dataset_ent.select(range(amt_needed_sentences))
+        sampled_sent_ent = sent_ent[:amt_needed_sentences]
+
+        # the remaining available "slots" go to sentences with no labels
+        sampled_data_no_ent = dataset_no_ent.select(range(size-amt_needed_sentences))
+        sampled_sent_no_ent = sent_no_ent[:(size-amt_needed_sentences)]
+
+        # merge back the two sampled data sets
+        sampled_data = concatenate_datasets([sampled_data_ent, sampled_data_no_ent], axis= 0)
+        sampled_sent = sampled_sent_ent + sampled_sent_no_ent
+        
+
+        # shuffle
+        
+        new_idx = [i for i in range(size)]
+        random.shuffle(new_idx)
+
+        sampled_data = sampled_data.select(new_idx)
+        sampled_sent = [sampled_sent[i] for i in new_idx]
+
+        return sampled_data, sampled_sent
 
 
