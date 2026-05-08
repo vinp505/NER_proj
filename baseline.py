e5bbb64b1f547a4d096d544a894b560ac38de6a3
vinp505 <pirasvincenzo05@gmail.com>
2026-05-08 13:55:10 +0200

    Revamped baseline and training script. Now language-specific fine-tuning will start from the baseline lora weights, which will then be updated and stored.

--- baseline.py
+++ baseline.py
@@ -1,63 +1,138 @@
+"""Script to carry out fine-tuning of a RoBERTa model on equal amount of senteces for each language to obtain the baseline model."""
+
+# ------------------------------------------------------------
+
 from tqdm.auto import tqdm
-from transformers import AutoTokenizer, AutoModelForTokenClassification, DataCollatorForTokenClassification, AutoConfig
+from functools import partial
+from transformers import AutoTokenizer, AutoModelForTokenClassification, DataCollatorForTokenClassification, AutoConfig, TrainingArguments, Trainer
 import torch
 from torch.utils.data import DataLoader
 import custom
 import argparse
 import evaluate
+import pathlib
+import os
+from peft import LoraConfig, TaskType, get_peft_model, PeftModel
 
 # ------------------------------------------------------------
 
 # add a parser, include needed arguments
-parser = argparse.ArgumentParser(description="Fine-tune baseline mBERT model")
-parser.add_argument("-o", "--output", help="Output file for test set predictions", required=True)
-parser.add_argument("-e", "--epochs", help="Number of fine-tuning epochs", required=True)
-parser.add_argument("-l", "--learnRate", help="Learning Rate", required=True)
-parser.add_argument("-b", "--batchSize", help="Batch Size", required=True)
-parser.add_argument("-f", "--finetune", help="Fine Tuning Method, can be: <to be added>", required=True)
+parser = argparse.ArgumentParser(description="Fine-tune baseline model")
+parser.add_argument("-e", "--epochs", help="Number of fine-tuning epochs", required=False)
+parser.add_argument("-lr", "--learnRate", help="Learning Rate", required=False)
+parser.add_argument("-b", "--batchSize", help="Batch Size", required=False)
+parser.add_argument("-f", "--finetune", help="Fine Tuning Method, can be: <to be added>", required=False)
+parser.add_argument("-v", "--verbose", help= "Boolean flag to indicate whether or not the script should periodically print status updates", required= False)
 args = parser.parse_args()
-
-# print main arguments
-print("Output file: ", args.output, "Number of epochs: ", args.epochs)
 
 # move to GPU if available
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
 # parameter specification
-MODEL_NAME = "google-bert/bert-base-multilingual-cased"
-LR = 3e-5
-EPOCHS = int(args.epochs)
-BATCH_SIZE = 8
+MODEL_NAME = "FacebookAI/xlm-roberta-base"
+BASELINE_FOLDER = "baseline"
+OUTPUT_DIR = pathlib.Path("baseline_model")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+EPOCHS = int(args.epochs) if args.epochs != None else 20
+LR = float(args.learnRate) if args.learnRate != None else 3e-5
+BATCH_SIZE = int(args.batchSize) if args.batchSize != None else 8
+FINETUNE_METHOD = args.finetune if args.finetune != None else "lora"
+VERBOSE = args.verbose if args.verbose != None else False
+
+# print main arguments
+print("Model folder: ", OUTPUT_DIR, "\nNumber of epochs: ", EPOCHS)
+
+# ------------------------------------------------------------
+
+if VERBOSE:
+    print("Loading data ...")
 
 # load data and split sets
 language_data = custom.LanguageData(MODEL_NAME)
-data_splitter = custom.DataSplit(language_data)
+data_splitter = custom.DataSplit(language_data, target_lang= "all")
 
-# obtain needed datasets
+# obtain needed dataset -> balanced across languages
 train_dataset = data_splitter.get_train_set()
-test_dataset = data_splitter.get_test_set("eng")
 
-# load model and configuration
+if VERBOSE:
+    print("Loading base model ...")
+
+# load model and configuration -> oob model + baseline lora weights
 multi_config = AutoConfig.from_pretrained(MODEL_NAME, num_labels=7)
 multi_model = AutoModelForTokenClassification.from_pretrained(
     MODEL_NAME,
     config=multi_config
 )
-multi_model.to(device)
 
-# load tokenizer, collator, and optimizer
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
-multi_data_collator = DataCollatorForTokenClassification(tokenizer)
-optimizer = torch.optim.AdamW(multi_model.parameters(), lr=LR)
+# ------------------------------------------------------------
 
-# create dataloader objects to iterate through batches
-train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=multi_data_collator)
-test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=multi_data_collator)
+#____ADD SOME IF STATEMENT HERE TO CHECK IF THE METHOD IS LORA or FULL FINETUNE etc...
+if FINETUNE_METHOD.lower() == "lora":
 
-metric = evaluate.load("seqeval")
+    if VERBOSE:
+        print("Setting up LoRA finetuning ...")
+    
+    # configure lora finetuning
+    peft_config = LoraConfig(
+        task_type=TaskType.TOKEN_CLS,
+        r=8,
+        lora_alpha=32,
+        init_lora_weights="gaussian",
+        target_modules=["query", "key", "value", "dense"]
+    )
+    peft_model = get_peft_model(multi_model, peft_config)
+    peft_model.print_trainable_parameters()
+    peft_model.to(device)
 
-# main part: train - eval
-#custom.train_model(multi_model, train_dataloader, optimizer, EPOCHS)
-custom.eval_model(multi_model, test_dataloader, metric, language_data.idx2tag)
-custom.save_predictions(multi_model, test_dataloader, tokenizer, args.output, language_data.idx2tag)
+    if VERBOSE:
+        print("Setting up baseline model finetuning ...")
+        
+    # load tokenizer, collator, and optimizer
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+    multi_data_collator = DataCollatorForTokenClassification(tokenizer)
+
+    # create dataloader objects to iterate through batches
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=multi_data_collator)
+
+    training_args = TrainingArguments(
+        output_dir=OUTPUT_DIR,
+        learning_rate=LR,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
+        num_train_epochs=EPOCHS,
+        save_strategy="epoch",
+        eval_strategy="no",
+        do_eval= False,
+        logging_strategy="epoch",
+        load_best_model_at_end=False
+    )
+
+    trainer = Trainer(
+        model=peft_model,
+        args=training_args,
+        train_dataset=train_dataset,
+        processing_class=tokenizer,
+        data_collator=multi_data_collator
+    )
+
+    if VERBOSE:
+        print(f"Finetuning baseline model ...")
+
+    # train and save the model
+    trainer.train()
+    peft_model.save_pretrained(OUTPUT_DIR)
+    
+    # rename checkpoint files to be more human readable
+    checkpoints = [d for d in os.listdir(OUTPUT_DIR) if d.startswith("checkpoint-")]
+    checkpoints.sort(key=lambda x: int(x.split("-")[1]))
+
+    for i, checkpoint in enumerate(checkpoints, 1):
+        old_path = os.path.join(OUTPUT_DIR, checkpoint)
+        new_path = os.path.join(OUTPUT_DIR, f"epoch_{i}")
+        os.rename(old_path, new_path)
+
+    print("Baseline model finetuned and saved.")
+
+else:
+    print("For now, the only supported finetune method is \"lora\".")
--- training.py
+++ training.py
@@ -1,3 +1,7 @@
+"""Script to carry out language-specific fine-tuning of the baseline RoBERTa model (previously fine-tuned on equal amount of senteces for each language)."""
+
+# ------------------------------------------------------------
+
 from tqdm.auto import tqdm
 from functools import partial
 from transformers import AutoTokenizer, AutoModelForTokenClassification, DataCollatorForTokenClassification, AutoConfig, TrainingArguments, Trainer
@@ -7,23 +11,22 @@ import custom
 import argparse
 import evaluate
 import pathlib
-from peft import LoraConfig, TaskType, get_peft_model
+import os
+from peft import LoraConfig, TaskType, get_peft_model, PeftModel
 
 # ------------------------------------------------------------
 
 # add a parser, include needed arguments
-parser = argparse.ArgumentParser(description="Fine-tune baseline mBERT model")
-parser.add_argument("-l", "--language", help="Fine tuning target language code, e.g., 'de' for German, 'all' for all languages", required=True)
-parser.add_argument("-o", "--output", help="Output folder for trained model and evaluation results", required=True)
-parser.add_argument("-e", "--epochs", help="Number of fine-tuning epochs", required=True)
-parser.add_argument("-lr", "--learnRate", help="Learning Rate", required=True)
-parser.add_argument("-b", "--batchSize", help="Batch Size", required=True)
-parser.add_argument("-f", "--finetune", help="Fine Tuning Method, can be: <to be added>", required=True)
-parser.add_argument("-k", "--kNonTarget", help="Number of training examples to include from the non-target languages.", required=True)
+parser = argparse.ArgumentParser(description="Fine-tune model")
+parser.add_argument("-l", "--language", help="Fine tuning target language code: 'eng', 'slk', 'dan', 'rom', 'chi', or 'all' for all languages", required=True)
+parser.add_argument("-o", "--output", help="Output folder for trained model", required=False)
+parser.add_argument("-e", "--epochs", help="Number of fine-tuning epochs", required=False)
+parser.add_argument("-lr", "--learnRate", help="Learning Rate", required=False)
+parser.add_argument("-b", "--batchSize", help="Batch Size", required=False)
+parser.add_argument("-f", "--finetune", help="Fine Tuning Method, can be: <to be added>", required=False)
+parser.add_argument("-k", "--kNonTarget", help="Number of training examples to include from the non-target languages", required=False)
+parser.add_argument("-v", "--verbose", help= "Boolean flag to indicate whether or not the script should periodically print status updates", required= False)
 args = parser.parse_args()
-
-# print main arguments
-print("Output folder: ", args.output, "Number of epochs: ", args.epochs)
 
 # move to GPU if available
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -31,45 +34,62 @@ print(f"Using device: {device}")
 
 # parameter specification
 MODEL_NAME = "FacebookAI/xlm-roberta-base"
-LR = float(args.learnRate)
-EPOCHS = int(args.epochs)
-BATCH_SIZE = int(args.batchSize)
-OUTPUT_DIR = pathlib.Path(args.output)
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-MODEL_OUTPUT_PATH = OUTPUT_DIR / (MODEL_NAME.split("/")[-1] + "_finetuned_" + args.language)
-K = int(args.kNonTarget)
+BASELINE_FOLDER = "baseline_model"
 TARGET_LANG = args.language
-FINETUNE_METHOD = args.finetune
+OUTPUT_DIR = pathlib.Path(args.output) if args.output != None else pathlib.Path(f"finetuned_models")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_OUTPUT_PATH = OUTPUT_DIR / (f"finetuned_model_{TARGET_LANG}")
+EPOCHS = int(args.epochs) if args.epochs != None else 20
+LR = float(args.learnRate) if args.learnRate != None else 3e-5
+BATCH_SIZE = int(args.batchSize) if args.batchSize != None else 8
+FINETUNE_METHOD = args.finetune if args.finetune != None else "lora"
+K = int(args.kNonTarget) if args.kNonTarget != None else 10
+VERBOSE = args.verbose if args.verbose != None else False
+
+# print main arguments
+print("Output folder: ", OUTPUT_DIR, "\nModel folder: ", MODEL_OUTPUT_PATH, "\nNumber of epochs: ", EPOCHS)
+
+# ------------------------------------------------------------
+
+if VERBOSE:
+    print("Loading data ...")
 
 # load data and split sets
 language_data = custom.LanguageData(MODEL_NAME)
 data_splitter = custom.DataSplit(language_data, target_lang=TARGET_LANG, k=K)
 
 # obtain needed datasets
-train_dataset = data_splitter.get_train_set()#assembles correct training set for the requested target language(s)
-test_dataset = data_splitter.get_test_set("eng")#in the training script we just do a small evaluation on only the english
+train_dataset = data_splitter.get_train_set() #assembles correct training set for the requested target language(s)
 
-# load model and configuration
+if VERBOSE:
+    print("Loading base model ...")
+
+# load model and configuration -> oob model
 multi_config = AutoConfig.from_pretrained(MODEL_NAME, num_labels=7)
 multi_model = AutoModelForTokenClassification.from_pretrained(
     MODEL_NAME,
     config=multi_config
 )
+# ------------------------------------------------------------
 
 #____ADD SOME IF STATEMENT HERE TO CHECK IF THE METHOD IS LORA or FULL FINETUNE etc...
-if FINETUNE_METHOD == "lora":
-    peft_config = LoraConfig(
-        task_type=TaskType.TOKEN_CLS,
-        r=8,
-        lora_alpha=32,
-        init_lora_weights="gaussian",
-        target_modules=["query", "key", "value", "dense"]
+if FINETUNE_METHOD.lower() == "lora":
+
+    if VERBOSE:
+        print("Setting up LoRA finetuning ...")
+    
+    # obtain baseline model (lora)
+    peft_model = PeftModel.from_pretrained(
+        multi_model, 
+        BASELINE_FOLDER, 
+        is_trainable=True
     )
-
-    peft_model = get_peft_model(multi_model, peft_config)
+    #peft_model.base_model.trainable = False
     peft_model.print_trainable_parameters()
-
     peft_model.to(device)
+
+    if VERBOSE:
+        print("Setting up model finetuning ...")
 
     # load tokenizer, collator, and optimizer
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
@@ -99,9 +119,23 @@ if FINETUNE_METHOD == "lora":
         data_collator=multi_data_collator
     )
 
+    if VERBOSE:
+        print(f"Finetuning model ...")
+        
+    # train and save the model
     trainer.train()
-
     peft_model.save_pretrained(MODEL_OUTPUT_PATH)
+    
+    # rename checkpoint files to be more human readable
+    checkpoints = [d for d in os.listdir(MODEL_OUTPUT_PATH) if d.startswith("checkpoint-")]
+    checkpoints.sort(key=lambda x: int(x.split("-")[1]))
+
+    for i, checkpoint in enumerate(checkpoints, 1):
+        old_path = os.path.join(MODEL_OUTPUT_PATH, checkpoint)
+        new_path = os.path.join(MODEL_OUTPUT_PATH, f"epoch_{i}")
+        os.rename(old_path, new_path)
+
+    print("Model finetuned and saved.")
 
 else:
     print("For now, the only supported finetune method is \"lora\".")
