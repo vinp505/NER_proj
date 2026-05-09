ee3e3afc95ee4fd513862d370a4bcc2437982c06
vinp505 <pirasvincenzo05@gmail.com>
2026-05-09 23:18:19 +0200

    i

--- evaluation.py
+++ evaluation.py
@@ -37,7 +37,7 @@ else:
     MODEL_DIR = pathlib.Path(args.modelFolder) if args.modelFolder != None else pathlib.Path(f"finetuned_models")
     MODEL_FOLDER = MODEL_DIR / (f"finetuned_model_{TARGET_LANG}")
 
-VERBOSE = args.verbose if args.verbose != None else False
+VERBOSE = bool(args.verbose) if args.verbose != None else False
 
 # print main arguments
 print("Model folder: ", MODEL_FOLDER)
@@ -49,8 +49,8 @@ if VERBOSE:
     print("Loading data ...")
 
 # load data and split sets
-language_data = custom.LanguageData(MODEL_NAME)
-data_splitter = custom.DataSplit(language_data)
+language_data = custom.LanguageData(MODEL_NAME, verbose= VERBOSE)
+data_splitter = custom.DataSplit(language_data, verbose= VERBOSE)
 
 # obtain needed datasets
 test_dataset = data_splitter.get_test_set()
@@ -62,28 +62,43 @@ multi_model = AutoModelForTokenClassification.from_pretrained(
     config=multi_config
 )
 
+# load tokenizer, collator, and eval module
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+multi_data_collator = DataCollatorForTokenClassification(tokenizer)
+metric = evaluate.load("seqeval")
+
+if VERBOSE:
+    print("Loading model ...")
+
 # obtain all epoch folders from model folder
 epochs = [e for e in os.listdir(MODEL_FOLDER) if e.startswith("epoch_")]
 epochs.sort(key=lambda x: int(x.split("_")[1]))
-
-# initialize lora model
-epoch_1_folder = os.path.join(MODEL_FOLDER, epochs[0])
-peft_model = PeftModel.from_pretrained(multi_model, epoch_1_folder)
 
 # iterate through epochs, evaluate each one
 for i, epoch in enumerate(epochs, 1):
 
     if VERBOSE:
         print(f"Evaluating: epoch {i}")
-
-    epoch_folder = os.path.join(MODEL_FOLDER, epoch)
     
     # load lora weights for the epoch
-    peft_model.delete_adapter("default")
-    peft_model.load_adapter(epoch_folder, adapter_name="default")
-    peft_model.set_adapter("default")
-
+    epoch_folder = os.path.join(MODEL_FOLDER, epoch)
+    peft_model = PeftModel.from_pretrained(multi_model, epoch_folder)
     peft_model.to(device)
-    peft_model.eval()
+    peft_model.eval()  # inside eval_model() as well
+    
+    # iterate through languages, obtain metrics for each one
+    for lang in language_data.lang_codes:
 
-    # ADD ACTUAL EVAL SCRIPT AND SAVE RESULTS
+        if VERBOSE:
+            print(f" > {lang}")
+            
+        test_set_l = test_dataset[lang]
+        dataloader = DataLoader(test_set_l, batch_size= 8, collate_fn= multi_data_collator)
+        custom.eval_model(peft_model, dataloader, metric, language_data.idx2tag, verbose= VERBOSE)
+
+        # ADD NICER PRINTING AND SAVE RESULTS
+    
+    # delete model, empty cache before starting new run
+    del peft_model
+    torch.cuda.empty_cache()
+
