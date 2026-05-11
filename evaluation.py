ce3c0f89b97c471536ec36a837a9f82568a9c176
vinp505 <pirasvincenzo05@gmail.com>
2026-05-11 11:27:17 +0200

    Added eval metrics storage in .csv format, added necessary files to queue eval scripts on the hpc.

--- evaluation.py
+++ evaluation.py
@@ -20,6 +20,7 @@ from peft import LoraConfig, TaskType, get_peft_model, PeftModel
 parser = argparse.ArgumentParser(description="Evaluate model")
 parser.add_argument("-l", "--language", help="Target language code of the model to be evaluated: 'all' (baseline model), 'eng', 'slk', 'dan', 'rom', 'chi'", required=True)
 parser.add_argument("-m", "--modelFolder", help="Folder containing the model (folder) to be evaluated", required=False)
+parser.add_argument("-d", "--evalDirectory", help="Directory in which to save the evaluation results", required=False)
 parser.add_argument("-v", "--verbose", help= "Boolean flag to indicate whether or not the script should periodically print status updates", required= False)
 args = parser.parse_args()
 
@@ -37,6 +38,8 @@ else:
     MODEL_DIR = pathlib.Path(args.modelFolder) if args.modelFolder != None else pathlib.Path(f"finetuned_models")
     MODEL_FOLDER = MODEL_DIR / (f"finetuned_model_{TARGET_LANG}")
 
+EVAL_DIR = pathlib.Path(args.evalDirectory) if args.evalDirectory != None else pathlib.Path("evaluation_results")
+EVAL_DIR.mkdir(parents=True, exist_ok=True)
 VERBOSE = bool(args.verbose) if args.verbose != None else False
 
 # print main arguments
@@ -74,8 +77,25 @@ if VERBOSE:
 epochs = [e for e in os.listdir(MODEL_FOLDER) if e.startswith("epoch_")]
 epochs.sort(key=lambda x: int(x.split("_")[1]))
 
-# initialize dictionary to store results
+
+# initialize dictionaries to store results
+
 lang_eval_data = {}
+lang_f1_records = {}
+
+for lang in language_data.lang_codes:
+
+    lang_eval_data[lang] = {
+        "F1" : [],
+        "Precision" : [],
+        "Recall" : [],
+        "Accuracy" : []
+    }
+    lang_f1_records[lang] = {
+        "High" : {"F1" : -0.1, "Epoch" : None},
+        "Low" : {"F1" : 1.1, "Epoch" : None}
+    }
+
 # iterate through epochs, evaluate each one
 for i, epoch in enumerate(epochs, 1):
 
@@ -94,38 +114,50 @@ for i, epoch in enumerate(epochs, 1):
         if VERBOSE:
             print(f" > {lang}")
 
-        # initialize metrics dict for the language
-        lang_eval_data[lang] = {
-            "F1" : [],
-            "Precision" : [],
-            "Recall" : [],
-            "Accuracy" : []
-        }
-
+        # obtain metrics
         test_set_l = test_dataset[lang]
         dataloader = DataLoader(test_set_l, batch_size= 8, collate_fn= multi_data_collator)
         metrics = custom.eval_model(peft_model, dataloader, eval_module, language_data.idx2tag, verbose= VERBOSE)
+        
 
         # store metrics
+        
         for k, v in metrics.items():
             lang_eval_data[lang][k].append(v)
+        
+        if metrics['F1'] < lang_f1_records[lang]["Low"]["F1"]:
+            lang_f1_records[lang]["Low"]["F1"] = metrics['F1']
+            lang_f1_records[lang]["Low"]["Epoch"] = i
+        
+        if metrics['F1'] > lang_f1_records[lang]["High"]["F1"]:
+            lang_f1_records[lang]["High"]["F1"] = metrics['F1']
+            lang_f1_records[lang]["High"]["Epoch"] = i
+            
     
     # delete model, empty cache before starting new run
     del peft_model
     torch.cuda.empty_cache()
 
-# print results if needed
-if VERBOSE:
-    print("\n\n")
+# print results
+print("\n\n")
+for lang, data in lang_eval_data.items():
+    print("\n+" + "-"*63 + "+")
+    print(f"|{('Language: ' + lang):^63}|")
+    print("+" + "-"*63 + "+")
+    print(f"|{f'Highest F1: {lang_f1_records[lang]['High']['F1']:.3f} (Epoch {lang_f1_records[lang]['High']['Epoch']})  |  Lowest F1: {lang_f1_records[lang]['Low']['F1']:.3f} (Epoch {lang_f1_records[lang]['Low']['Epoch']})':^63}|")
+    print("+" + "-"*63 + "+")
+    print(f"|{'Epoch':^11}|{'F1':^12}|{'Precision':^12}|{'Recall':^12}|{'Accuracy':^12}|")
+    print("+" + "-"*11 + ('+' + "-"*12)*4 + '+')
+
+    for i in range(len(epochs)):
+        print(f"|{(i+1):^11}|{data['F1'][i]:>12.3f}|{data['Precision'][i]:>12.3f}|{data['Recall'][i]:>12.3f}|{data['Accuracy'][i]:>12.3f}|")
+    print("+" + "-"*63 + "+")
+
+# save results in csv format
+with open(str(EVAL_DIR) + f'/evaluation_ftmodel_{TARGET_LANG}.csv') as f:
+    
+    f.write("lang,epoch,F1,precision,recall,accuracy\n")
+
     for lang, data in lang_eval_data.items():
-        print("\n+" + "-"*63 + "+")
-        print(f"|{('Language: ' + lang):^63}|")
-        print("+" + "-"*63 + "+")
-        print(f"|{'Epoch':^11}|{'F1':^12}|{'Precision':^12}|{'Recall':^12}|{'Accuracy':^12}|")
-        print("+" + "-"*11 + ('+' + "-"*12)*4 + '+')
-
         for i in range(len(epochs)):
-            print(f"|{(i+1):^11}|{data['F1'][i]:>12.3f}|{data['Precision'][i]:>12.3f}|{data['Recall'][i]:>12.3f}|{data['Accuracy'][i]:>12.3f}|")
-        print("+" + "-"*63 + "+")
-
-# ADD METHOD TO SAVE RESULTS TO FILE
+            f.write(f"{lang},{(i+1)},{data['F1'][i]:.3f},{data['Precision'][i]:.3f},{data['Recall'][i]:.3f},{data['Accuracy'][i]:.3f}\n")
---
+++ queueEval.py
@@ -0,0 +1,83 @@
+import paramiko
+import getpass
+import time
+import os
+from scp import SCPClient
+
+#set hyperparameters
+targetLanguages = ["eng", "slk", "dan", "rom", "chi"]#all languages that we wish to train models for
+#set an experiment name (just derive it from the parameters) -> all stuff related to this training run will be saved here
+experimentName = f"NER_models_evaluation"
+outputDir = f"~/NER_proj/{experimentName}"
+
+#login
+username = input("Username: ")
+password = getpass.getpass('Password: ')
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('hpc.itu.dk', username=username, password=password)
+print(f"Successfully logged in as {username}.")
+
+# Upload the .job file -> such that edits are directly transferred without having to do a git pull on the hpc
+local_job_file = "hpc/evalModel.job"
+# with SCPClient(ssh.get_transport()) as scp:
+#     scp.put(local_job_file, "~/NER_proj/hpc/trainModel.job")
+
+lang2JobId = {}#map from target language to job id
+for language in targetLanguages:
+    #construct the command: (has to be in one go since each call to ssh.exec_command() produces a new session)
+    command = (f"cd ~/NER_proj/hpc && "
+                f"TARGET_LANG={language} "
+                "sbatch trainModel.job")
+    # Submit the job
+    stdin, stdout, stderr = ssh.exec_command(command)
+    err = stderr.read().decode()
+    if err:#if it's not the empty string, i.e. we got an error:
+        print("STDERR:", err)
+    job_id = int(stdout.read().decode().split()[-1])
+    lang2JobId[language] = job_id
+
+print("Submitted all jobs.")
+print("Monitoring the situation closely ;)")
+
+def get_job_state(ssh, job_id):
+    _, stdout, _ = ssh.exec_command(f'sacct -j {job_id} --format=State --noheader')
+    lines = stdout.read().decode().strip().split('\n')
+    for line in lines:
+        line = line.strip()
+        if line:
+            return line  # Return first non-empty state
+    return "UNKNOWN"
+
+#all the states in which a job is not running anymore
+TERMINAL_STATES = {'COMPLETED', 'FAILED', 'CANCELLED', 'TIMEOUT', 
+                   'NODE_FAIL', 'DEADLINE', 'BOOT_FAIL', 'OUT_OF_MEMORY'}
+
+def all_done(states:dict) -> bool:
+    return all([(state in TERMINAL_STATES) for state in states.values()])#only return true if all jobs are completed
+
+while True:
+    states = {lang: get_job_state(ssh, lang2JobId[lang]) for lang in targetLanguages}
+    if all_done(states):
+        print("All jobs terminated.")
+        break
+    #print status for all languages
+    for lang in targetLanguages:
+        print(f"{lang2JobId[lang]}({lang}):\tSTATUS: {states[lang]}")
+    print("______________________________________")
+
+print("End of Program.")
+
+# # Download final outputs
+# current_directory = os.getcwd()
+# slurm_out = f"slurm-{job_id}.out"
+# with SCPClient(ssh.get_transport()) as scp:
+#     scp.get(slurm_log_path, os.path.join(current_directory, slurm_out))
+# print(f"Downloaded Slurm log to {slurm_out}")
+
+# local_output_filename = "final_predictions.iob2"
+# with SCPClient(ssh.get_transport()) as scp:
+#     scp.get(outputFile, local_output_filename)
+# print(f'Downloaded output file to {local_output_filename}')
+
+ssh.close()
